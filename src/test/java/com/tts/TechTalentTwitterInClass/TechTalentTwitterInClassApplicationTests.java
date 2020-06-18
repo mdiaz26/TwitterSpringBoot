dc63558490ccbcec2faef24ebc2a8c1ae0260946
mdiaz26 <mike.diaz006@gmail.com>
2020-06-18 09:48:15 -0400

    first commit

---
+++ src/test/java/com/tts/TechTalentTwitterInClass/TechTalentTwitterInClassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tts.TechTalentTwitterInClass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechTalentTwitterInClassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
